2f80e5c80fed1e8dcf8b7c479855991e2a61f545
ashish-greycube <mr.ashish.shah@gmail.com>
2018-08-07 11:01:38 +0530

    changes told by jignesh

--- origintheme/api.py
+++ origintheme/api.py
@@ -10,7 +10,7 @@ def on_session_creation(login_manager):
 	info = frappe.db.get_value("User", frappe.local.session_obj.user,
 			["home_page_link"], as_dict=1)
 
-	frappe.local.response["home_page"] = info.home_page_link or "/desk"
+	frappe.local.response["home_page"] = info.home_page_link or "/desk#home-page"
 
 @frappe.whitelist()
 def get_sidebar_template():
